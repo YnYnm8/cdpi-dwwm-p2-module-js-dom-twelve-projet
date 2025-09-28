da5de3b51a3d171e2fd19e51fd5e3d7d965dec81
Meiko Yahyaoui <meikotoulouse@gmail.com>
2025-09-28 17:57:24 +0200

    darkmode

--- 3.1 - dark mode mutiple boutons/script.js
+++ 3.1 - dark mode mutiple boutons/script.js
@@ -1,6 +1,23 @@
 
+const iconArr = document.querySelectorAll(".icon");
 //1. Je recupere tout les switchs
 
+const switchArr = document.querySelectorAll(".switch");
 //2. Je recupere tout les elements html de ma page
-
+const allElemArr = document.querySelectorAll(" * ");
 //3. Je parcours tout les switchs
+
+switchArr.forEach(function (switchElem) {
+    switchElem.addEventListener("click", function () {
+
+        allElemArr.forEach(function (allElem) {
+            allElem.classList.toggle("darkmode");
+        })
+        iconArr.forEach(function (iconElem) {
+
+            iconElem.classList.toggle("fa-moon");
+        })
+    })
+})
+
+
