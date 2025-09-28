22ab5e2c6ce62248473e99fbb93ed7040554f681
Meiko Yahyaoui <meikotoulouse@gmail.com>
2025-09-28 20:16:01 +0200

    burger

--- 4 - menuburger/script.js
+++ 4 - menuburger/script.js
@@ -3,3 +3,8 @@
 const burger = document.querySelector(".burger");
 // 2. Je recupere la fenetre modal
 const modal = document.querySelector(".modal");
+
+burger.addEventListener("click",function(){
+    modal.classList.toggle("show-modal");
+    burger.classList.toggle("fa-x");
+})
