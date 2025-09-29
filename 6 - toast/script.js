10dd378ac8da7ad48f83360563e539f8ae4cced2
Meiko Yahyaoui <meikotoulouse@gmail.com>
2025-09-29 11:32:00 +0200

    toast

--- 6 - toast/script.js
+++ 6 - toast/script.js
@@ -11,3 +11,21 @@
 const btn = document.querySelector("button");
 // 2. Je recupere le conteneur de toasts
 const ctnToasts = document.querySelector(".container-toasts");
+
+
+
+btn.addEventListener("click", function () {
+    const newDivElem = document.createElement("div");
+    newDivElem.classList.add("toast");
+    newDivElem.innerHTML = "<p>Votre fichier est enregister !</p>";
+    ctnToasts.appendChild(newDivElem);
+
+    newDivElem.addEventListener("click", function () {
+        newDivElem.remove();
+    });
+
+    setTimeout(function () {
+        newDivElem.remove();
+    }, 3000);
+})
+
